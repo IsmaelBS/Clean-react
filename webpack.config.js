0e9183f778ae0430b199469ccc2733615f252da6
Ismael Barboza dos Santos <ismael@inovadora.com>
2021-09-13 09:15:27 -0300

    chore: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: 'src/main/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public', 'js'),
+    publicPath: 'public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['tsx', 'ts', 'js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+}
