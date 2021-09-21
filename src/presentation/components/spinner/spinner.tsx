c4f6f1ab94e56a3728514c1abfda310f074c59dc
Ismael Barboza dos Santos <ismael@inovadora.com>
2021-09-21 09:45:00 -0300

    refactor: move Form-status for a separate file

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -3,8 +3,10 @@ import Styles from './spinner-styles.scss'
 
 type Props = React.HtmlHTMLAttributes<HTMLElement>
 
-export const Spinner: React.FC<Props> = (props: Props) => {
+const Spinner: React.FC<Props> = (props: Props) => {
   return (
     <div {...props} className={[Styles.spinner, props.className].join(' ')}><div></div><div></div><div></div><div></div></div>
   )
 }
+
+export default Spinner
