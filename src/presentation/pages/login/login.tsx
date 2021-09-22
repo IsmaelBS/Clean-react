ae5ff4ea0feb5e6a11901713ef12c2d6a185c5fb
Ismael Barboza dos Santos <ismael@inovadora.com>
2021-09-22 09:23:34 -0300

    feat: ensure Login will not render on load

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import { Footer, LoginHeader, Input, FormStatus } from '@/presentation/components'
+import Context from '@/presentation/context/form/form-context'
 
-const Login: React.FC = () => (
-  <div className={Styles.login}>
-    <LoginHeader />
-    <form className={Styles.form}>
-      <h2>login</h2>
-      <Input type="email" name="email" placeholder="Digite seu e-mail" />
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
-      <Input type="password" name="password" placeholder="Digite sua senha" />
+const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
 
-      <button className={Styles.submit} type="submit">Entrar</button>
-      <span className={Styles.link}>Criar conta</span>
-      <FormStatus />
-    </form>
-    <Footer />
-  </div>
-)
+  return (
+    <div className={Styles.login}>
+      <LoginHeader />
+      <Context.Provider value={state}>
+        <form className={Styles.form}>
+          <h2>login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+
+          <button className={Styles.submit} type="submit">Entrar</button>
+          <span className={Styles.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  )
+}
 
 export default Login
