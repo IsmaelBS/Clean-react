4fb7f65ddcca5cafd4806f14a56a959419e945dd
Ismael Barboza dos Santos <ismael@inovadora.com>
2021-09-22 08:06:01 -0300

    feat: add router

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Footer from '@/presentation/components/footer/footer'
-import Header from '@/presentation/components/login-header/login-header'
-import Input from '@/presentation/components/input/input'
-import FormStatus from '@/presentation/components/form-status/form-status'
 import Styles from './login-styles.scss'
+import { Footer, LoginHeader, Input, FormStatus } from '@/presentation/components'
 
 const Login: React.FC = () => (
   <div className={Styles.login}>
-    <Header />
+    <LoginHeader />
     <form className={Styles.form}>
       <h2>login</h2>
       <Input type="email" name="email" placeholder="Digite seu e-mail" />
