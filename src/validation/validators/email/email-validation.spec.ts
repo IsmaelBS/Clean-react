d8aeb1cb0b174d0773916ed563c379aaef85e59f
Ismael Barboza dos Santos <ismael@inovadora.com>
2021-12-06 10:50:10 -0300

    test: ensure EmailValidation retuns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,20 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from '@/validation/validators/email/email-validation'
 
 describe('EmailValidation', () => {
+  const makeSut = (): EmailValidation => {
+    return new EmailValidation(faker.database.column())
+  }
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = makeSut()
+    const error = sut.validate(faker.random.words())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
