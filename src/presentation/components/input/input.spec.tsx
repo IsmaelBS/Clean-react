7ac9a206e7360b9ffaa76a8ad7531d40c1198e77
Ismael Barboza <ismabarboza@gmail.com>
2022-03-28 08:49:55 -0300

    refactor: move SUT for a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presentation/context/form/form-context'
+
+const makeSut = (fieldName: string): HTMLElement => {
+  render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name={fieldName} />
+    </Context.Provider>
+  )
+  return screen.getByTestId(fieldName)
+}
 describe('Input Component', () => {
   test('Should begin with readOnly', () => {
-    render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name='field' />
-      </Context.Provider>
-    )
-    const input = screen.getByTestId('field') as HTMLInputElement
-    expect(input.readOnly).toBe(true)
+    const sut = makeSut('field') as HTMLInputElement
+    expect(sut.readOnly).toBe(true)
   })
 })
