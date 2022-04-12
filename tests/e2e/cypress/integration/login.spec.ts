d764def590bba20e3f6cb31ba059da49af993a33
Ismael Barboza <ismabarboza@gmail.com>
2022-04-12 13:24:13 -0300

    test: ensure not present error if form is valid

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -44,4 +44,25 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('Should not present error state if form is valid', () => {
+    cy.getByTestId('email')
+      .focus()
+      .type(faker.internet.email())
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('have.text', '✅')
+
+    cy.getByTestId('password')
+      .focus()
+      .type(faker.random.alphaNumeric(5))
+
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Tudo certo!')
+      .should('have.text', '✅')
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
 })
