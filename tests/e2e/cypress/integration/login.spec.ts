ec10f27e354a3c160082c87130824e2919b216cc
Ismael Barboza <ismabarboza@gmail.com>
2022-04-13 09:37:46 -0300

    test: ensure Login will not submit form if form is invalid

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -224,4 +224,26 @@ describe('Login', () => {
     cy.getByTestId('submit').dblclick()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: /login/
+      },
+      {
+        statusCode: 200,
+        body: {
+          accessToken: faker.random.words()
+        }
+      }
+    ).as('request')
+
+    cy.getByTestId('email')
+      .focus()
+      .type(faker.internet.email())
+      .type('{enter}')
+
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
